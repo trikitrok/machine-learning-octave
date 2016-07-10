25467cc79effbb5effbcd13e759271c0f5d62a68
trikitrok <trikitrok@gmail.com>
2016-07-10 17:16:50 +0200

    Neural Network Prediction Function working

--- Week4/machine-learning-ex3/ex3/predict.m
+++ Week4/machine-learning-ex3/ex3/predict.m
@@ -20,16 +20,16 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+X = [ones(m, 1) X];
+z2 = Theta1 * X';
+a2 = sigmoid(z2');
 
+a2 = [ones(m, 1) a2];
+z3 = Theta2 * a2';
+a3 = sigmoid(z3');
 
-
-
-
-
-
-
+[x, ix] = max(a3, [], 2);
+p = ix;
 
 % =========================================================================
-
-
 end
