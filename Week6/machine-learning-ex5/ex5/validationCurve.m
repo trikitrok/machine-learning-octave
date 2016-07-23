156f0d1e642192dc01a7331fdde659cd4e9dbcda
trikitrok <trikitrok@gmail.com>
2016-07-23 20:14:49 +0200

    Validation Curve working

--- Week6/machine-learning-ex5/ex5/validationCurve.m
+++ Week6/machine-learning-ex5/ex5/validationCurve.m
@@ -38,15 +38,19 @@ error_val = zeros(length(lambda_vec), 1);
 %       end
 %
 %
+m = size(Xval);
 
+for i = 1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    % Compute train / val errors when training linear 
+    % regression with regularization parameter lambda
+    % You should store the result in error_train(i)
+    % and error_val(i)
+    [theta] = trainLinearReg(X, y, lambda);
 
-
-
-
-
-
-
-
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+    error_train(i) = linearRegCostFunction(X, y, theta, 0);
+end
 
 % =========================================================================
 
