9e053be1e896fb83bf9e1ee129ab33b01a0bff57
trikitrok <trikitrok@gmail.com>
2016-08-06 05:25:07 +0200

    Compute Centroid Means (k-Means) working

--- Week8/machine-learning-ex7/ex7/computeCentroids.m
+++ Week8/machine-learning-ex7/ex7/computeCentroids.m
@@ -16,7 +16,6 @@ function centroids = computeCentroids(X, idx, K)
 % You need to return the following variables correctly.
 centroids = zeros(K, n);
 
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Go over every centroid and compute mean of all points that
 %               belong to it. Concretely, the row vector centroids(i, :)
@@ -26,7 +25,17 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
+for k = 1:K
+  sum = zeros(1, n);
+  q = 0;
+  for i = 1:m
+    if (idx(i) == k)
+      sum = sum + X(i, :);
+      q = q + 1;
+    end
+  end
+  centroids(k, :) = sum ./ q;
+end
 
 
 
